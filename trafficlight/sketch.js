fe93658b222dfd14b578b362197be4e9f72b34ea
Aaraiz-A <161645440+Aaraiz-A@users.noreply.github.com>
2024-03-01 13:28:44 -0600

    I added traffic light

---
+++ trafficlight/sketch.js
@@ -0,0 +1,75 @@
+// Traffic Light Starter Code
+// Your Name Here
+// The Date Here
+
+// GOAL: make a 'traffic light' simulator. For now, just have the light
+// changing according to time. You may want to investigate the millis()
+// function at https://p5js.org/reference/#/p5/millis
+
+function setup() {
+  createCanvas(100, 300);
+}
+
+function drawRedLight() {
+  fill("red");
+  ellipse(width / 2, height / 2 - 65, 50, 50); //top
+}
+
+let state = "red";
+let lastTimeSwitched = 0;
+let redDuration = 2000;
+let yellowDuration = 1000;
+let greenDuration = 2500;
+
+function draw() {
+  background(255);
+  drawOutlineOfLights();
+  determineState();
+  drawCorrectLight();
+}
+
+function determineState() {
+  if (state === "red" && millis() > lastTimeSwitched + redDuration) {
+    lastTimeSwitched = millis();
+    state = "green";
+  } else if (state === "green" && millis() > lastTimeSwitched + greenDuration) {
+    lastTimeSwitched = millis();
+    state = "yellow";
+  } else if ((state === "yellow") && (millis() > lastTimeSwitched + yellowDuration)) {
+    lastTimeSwitched = millis();
+    state = "red";
+  }
+}
+
+function drawCorrectLight() {
+  if (state === "green") {
+    drawGreenLight();
+  } else if (state === "yellow") {
+    drawYellowLight();
+  } else if (state === "red") {
+    drawRedLight();
+  }
+}
+
+function drawYellowLight() {
+  fill("yellow");
+  ellipse(width / 2, height / 2, 50, 50); //middle
+}
+
+function drawGreenLight() {
+  fill("green");
+  ellipse(width / 2, height / 2 + 65, 50, 50); //bottom
+}
+
+function drawOutlineOfLights() {
+  //box
+  rectMode(CENTER);
+  fill(0);
+  rect(width / 2, height / 2, 75, 200, 10);
+
+  //lights
+  fill(255);
+  ellipse(width / 2, height / 2 - 65, 50, 50); //top
+  ellipse(width / 2, height / 2, 50, 50); //middle
+  ellipse(width / 2, height / 2 + 65, 50, 50); //bottom
+}
