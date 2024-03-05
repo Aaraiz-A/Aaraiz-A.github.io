9490374b77a6569b4a4e523519d12b5808e9463e
Aaraiz-A <161645440+Aaraiz-A@users.noreply.github.com>
2024-03-05 14:05:03 -0600

    Making the score.
    I made the score relative to the amount of times you click stuff.

--- scene/sketch.js
+++ scene/sketch.js
@@ -5,7 +5,8 @@ let score1;
 
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1000, 500);
+  noCursor();
   radius = 100;
   x = random(width - radius);
   y = random(height - radius);
@@ -13,13 +14,16 @@ function setup() {
 }
 
 function draw() {
-  scoretext();
   background(220);
+  scoretext();
   circle1();
   circle2();
   circle3();
   circle4();
-  targetsscore();
+  crosshairs();
+  // targetsscore();
+  // maketargetmove();
+  // mouseWheel();
 }
 
 function circle1() {
@@ -46,24 +50,34 @@ function circle4() {
   circle(x, y, radius/4);
 }
 
-function targetsscore() {
-  if (mouseClicked(circle1)) {
+function mouseClicked() {
+  if (circle(x, y, radius)) {
     score1 = score1 + 1;
-  }
-  if (mouseClicked(circle2)) {
-    score1 = score1 + 2;
-  }
-  if (mouseClicked(circle3)) {
-    score1 = score1 + 3;
-  }
-  if (mouseClicked(circle4)) {
-    score1 = score1 + 5;
   }
 
 }
+
+// function maketargetmove() {
+//   if (mouseClicked(circle1 || circle2 || circle3 || circle4)) {
+//     x = random(width - radius);
+//     y = random(height - radius);
+//   }
+// }
 
 function scoretext() {
   fill("black");
-  text("Score:" + score1, 350, 20)
+  text("Score:" + score1, width - 50, height - 475);
 }
 
+function crosshairs() {
+  fill(0);
+  rect(mouseX, mouseY + 4, 1, 4);
+  rect(mouseX, mouseY - 7, 1, 4);
+  rect(mouseX + 4, mouseY, 4, 1);
+  rect(mouseX - 7,mouseY, 4, 1);
+}
+
+// function mouseWheel(event) {
+//   print(event.delta)
+//   radius += event.delta;
+// }
