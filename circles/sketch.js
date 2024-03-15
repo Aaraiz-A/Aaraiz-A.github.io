7d3c9026fdbfe9e2dca74d7ca0d2877a2f6ca4ba
Aaraiz-A <161645440+Aaraiz-A@users.noreply.github.com>
2024-03-15 13:21:07 -0600

    balls

--- circles/sketch.js
+++ circles/sketch.js
@@ -5,16 +5,7 @@ let ballArray = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
-  let ball = {
-    x: width/2,
-    y: height/2,
-    radius: random(15, 30),
-    color: color(random(255), random(255), random(255)),
-    dx: random(-5, 5),
-    dy: random(-5, 5),
-  };
-  ballArray.push(ball);
+  spawnBall(width/2, height/2);
 }
 
 function draw() {
@@ -23,43 +14,47 @@ function draw() {
   displayBalls();
 }
 
-function moveBalls() {
-  for (let ball of ballArray) {
-    ball.x += ball.dx;
-  }
-  ball.y += ball.dy;
-
-  //teleport across screen if needed...
-  if (ball.x < 0) {
-    ball.x = width;
-  }
-  else if (ball.x > width) {
-    ball.x = 0;
-  }
-  if (ball.y < 0) {
-    ball.y = height;
-  }
-  else if (ball.y > height) {
-    ball.y = 0;
-  }
-
-}
-
-function displayBalls() {
-  fill(ball.color);
-  circle(ball.x, ball.y, ball.radius*2);
-}
-
-function spawnBall(initialX, intialY) {
-  let ball = {
-    x: width/2,
-    y: height/2,
-    radius: random(15, 30),
-    color: color(random(255), random(255), random (255))
-  };
-}
-
-function mousePressed () {
+function mousePressed() {
   spawnBall(mouseX, mouseY);
 }
 
+function moveBalls() {
+  for (let ball of ballArray) {
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+  
+    //teleport across screen if needed...
+    if (ball.x < 0) {
+      ball.x = width;
+    }
+    else if (ball.x > width) {
+      ball.x = 0;
+    }
+  
+    if (ball.y < 0) {
+      ball.y = height;
+    }
+    else if (ball.y > height) {
+      ball.y = 0;
+    }
+  }
+}
+
+function displayBalls() {
+  for (let ball of ballArray) {
+    fill(ball.color);
+    circle(ball.x, ball.y, ball.radius * 2);
+  }
+}
+
+function spawnBall(initialX, initialY) {
+  let ball = {
+    x: initialX,
+    y: initialY,
+    radius: random(15, 30),
+    color: color(random(255), random(255), random(255)),
+    dx: random(-5, 5),
+    dy: random(-5, 5),
+  };
+  ballArray.push(ball);
+}
