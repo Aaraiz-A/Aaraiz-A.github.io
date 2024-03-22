6352c0055466dadd9e1f9560f3088a9424bba7a3
Aaraiz-A <161645440+Aaraiz-A@users.noreply.github.com>
2024-03-22 13:55:54 -0600

    did some weird 3d thingy

--- arraysp/sketch.js
+++ arraysp/sketch.js
@@ -13,14 +13,40 @@ function setup() {
 function draw() {
   background(220);
   maincharactercharacteristics();
+  displaymaincharacter();
+  maincharactermoves();
 }
 
 function maincharactercharacteristics() {
   let ball = {
     x: width/2,
     y: height,
-    diameter: 50
+    diameter: 50,
+    dx: 5,
+    dy: 5,
   };
-
   theBall.push(ball);
+}
+
+function displaymaincharacter() {
+  for (let ball of theBall) {
+    circle(ball.x, ball.y  - ball.diameter/2, ball.diameter);
+  }
+}
+
+function maincharactermoves() {
+  for (let ball of theBall) {
+    if (keyIsDown(87)) { //w
+      ball.y -= ball.dy;
+    }
+    if (keyIsDown(83)) { //s
+      ball.y += ball.dy;
+    }
+    if (keyIsDown(68)) { //d
+      ball.x += ball.dx;
+    }
+    if (keyIsDown(65)) { //a
+      ball.x -= ball.dx;
+    }
+  }
 }
