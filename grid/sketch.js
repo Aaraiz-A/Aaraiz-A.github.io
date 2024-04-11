8d4ac494fc140d275355406aeee138deeeaada91
Aaraiz-A <161645440+Aaraiz-A@users.noreply.github.com>
2024-04-11 14:11:19 -0600

    stuff im lost on

--- grid/sketch.js
+++ grid/sketch.js
@@ -1,6 +1,8 @@
 // 2D Grid
+// Dan Schellenberg
+// Apr 9, 2024
 
-// if you are hardcoding a level, I would use something like this
+// if you are hard-coding a level, I'd use something like this
 
 // let grid = [[1, 0, 0, 1],
 //             [0, 1, 0, 1],
@@ -11,16 +13,37 @@
 //             [0, 1, 0, 1],
 //             [0, 0, 0, 1]];
 
-// if randomizing the grid, do this:
 let grid;
-let cellsize;
+let cellSize;
 const GRID_SIZE = 10;
+let toggleStyle = "self";
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  grid = generaterandomgrid(GRID_SIZE, GRID_SIZE);
-  // this is dumb and we should check if this is the right size
-  cellsize = height/grid.length;
+  //make the canvas the largest square that you can...
+  if (windowWidth < windowHeight) {
+    createCanvas(windowWidth, windowWidth);
+  }
+  else {
+    createCanvas(windowHeight, windowHeight);
+  }
+
+  //if randomizing the grid, do this:
+  grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
+  
+  //this is dumb -- should check if this is the right size!
+  cellSize = height/grid.length;
+}
+
+function windowResized() {
+  //make the canvas the largest square that you can...
+  if (windowWidth < windowHeight) {
+    resizeCanvas(windowWidth, windowWidth);
+  }
+  else {
+    resizeCanvas(windowHeight, windowHeight);
+  }
+
+  cellSize = height/grid.length;
 }
 
 function draw() {
@@ -30,30 +53,72 @@ function draw() {
 
 function keyPressed() {
   if (key === "r") {
-    grid = generaterandomgrid(GRID_SIZE, GRID_SIZE);
+    grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
+  }
+
+  if (key === "e") {
+    grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
+  }
+
+  if (key === "n") {
+    toggleStyle = "neighbours";
+  }
+
+  if (key === "s") {
+    toggleStyle = "self";
+  }
+}
+
+function mousePressed() {
+  let x = Math.floor(mouseX/cellSize);
+  let y = Math.floor(mouseY/cellSize);
+
+  //toggle self
+  toggleCell(x, y);
+
+  // and NESW neighbours, if style is set to neighbours
+  if (toggleStyle === "neighbours") {
+    toggleCell(x + 1, y);
+    toggleCell(x - 1, y);
+    toggleCell(x, y + 1);
+    toggleCell(x, y - 1);
+  }
+}
+
+function toggleCell(x, y) {
+  // make sure the cell you're toggling is in the grid...
+  if (x < GRID_SIZE && y < GRID_SIZE &&
+      x >= 0 && y >= 0) {
+    //toggle the color of the cell
+    if (grid[y][x] === 0) {
+      grid[y][x] = 1;
+    }
+    else {
+      grid[y][x] = 0;
+    }
   }
 }
 
 function displayGrid() {
   for (let y = 0; y < grid.length; y++) {
-    for (let x = 0; x < grid[y].length; x ++) {
+    for (let x = 0; x < grid[y].length; x++) {
       if (grid[y][x] === 1) {
         fill("black");
       }
       else {
         fill("white");
       }
-      square(x * cellsize, y * cellsize, cellsize);
+      square(x * cellSize, y * cellSize, cellSize);
     }
   }
 }
 
-function generaterandomgrid(cols, rows) {
+function generateRandomGrid(cols, rows) {
   let emptyArray = [];
   for (let y = 0; y < rows; y++) {
     emptyArray.push([]);
     for (let x = 0; x < cols; x++) {
-      // half the time be a 1 and the other half be a 0
+      //half the time, be a 1. Other half, be a 0.
       if (random(100) < 50) {
         emptyArray[y].push(0);
       }
@@ -65,6 +130,13 @@ function generaterandomgrid(cols, rows) {
   return emptyArray;
 }
 
-function mouseClicked() {
-  
+function generateEmptyGrid(cols, rows) {
+  let emptyArray = [];
+  for (let y = 0; y < rows; y++) {
+    emptyArray.push([]);
+    for (let x = 0; x < cols; x++) {
+      emptyArray[y].push(0);
+    }
+  }
+  return emptyArray;
 }
