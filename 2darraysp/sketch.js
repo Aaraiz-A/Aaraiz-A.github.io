0fa8cbd3da24d426fee95182117d05c45fada462
Aaraiz-A <161645440+Aaraiz-A@users.noreply.github.com>
2024-04-23 14:11:10 -0600

    did basically nothing

--- 2darraysp/sketch.js
+++ 2darraysp/sketch.js
@@ -7,25 +7,23 @@
 let grid;
 let cellSize;
 const GRID_SIZE = 10;
-const pcols = 4;
-const prows = 9;
+const pcols = 9;
+const prows = 4;
 let startButtonCharacteristics;
 let startButtonTextCharacteristics;
 let state = "title screen";
 
 function setup() {
-  //make the canvas the largest square that you can...
   if (windowWidth < windowHeight) {
     createCanvas(windowWidth, windowWidth);
   }
   else {
     createCanvas(windowHeight, windowHeight);
   }
-  
-  //if randomizing the grid, do this:
-  grid = generateRandomGrid(pcols, prows);
+
+  grid = generateRandomGrid(prows, pcols);
     
-  //this is dumb -- should check if this is the right size!
+
   cellSize = height/grid.length;
 }
 
@@ -40,27 +38,12 @@ function draw() {
     displayStartButtonText();
   }
   else if (state === "periodic table") {
-    background(220);
+    background(0, 48, 73);
     displayGrid();
   }
 }
 
-// PERIODIC TABLE ELEMENTS
-function toggleCell(x, y) {
-  // make sure the cell you're toggling is in the grid...
-  if (x < GRID_SIZE && y < GRID_SIZE &&
-      x >= 0 && y >= 0) {
-    //toggle the color of the cell
-    if (grid[y][x] === 0) {
-      grid[y][x] = 1;
-    }
-    else {
-      grid[y][x] = 0;
-    }
-  }
-}
-
-
+// MAKING THE GRID 48-81
 function displayGrid() {
   for (let y = 0; y < grid.length; y++) {
     for (let x = 0; x < grid[y].length; x++) {
@@ -74,24 +57,12 @@ function generateRandomGrid(cols, rows) {
   for (let y = 0; y < rows; y++) {
     emptyArray.push([]);
     for (let x = 0; x < cols; x++) {
-      //half the time, be a 1. Other half, be a 0.
       if (random(100) < 50) {
         emptyArray[y].push(0);
       }
       else {
         emptyArray[y].push(1);
       }
-    }
-  }
-  return emptyArray;
-}
-
-function generateEmptyGrid(cols, rows) {
-  let emptyArray = [];
-  for (let y = 0; y < rows; y++) {
-    emptyArray.push([]);
-    for (let x = 0; x < cols; x++) {
-      emptyArray[y].push(0);
     }
   }
   return emptyArray;
@@ -154,3 +125,4 @@ function changeColorOfStartButton() {
     startButtonCharacteristics.b = 255;
   }
 }
+
